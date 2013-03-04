e7f75c89d941d093b25d255e0ca66e8efc997443
jlusdy <jlusdy@gmail.com>
2013-03-04 13:52:46 +0800

    修复比较方法的Bug，增加对象相等条件的判断。 Fixed #11

--- src/main/java/com/taobao/profile/analysis/TimeSortData.java
+++ src/main/java/com/taobao/profile/analysis/TimeSortData.java
@@ -4,7 +4,7 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
- * 
+ *
  */
 package com.taobao.profile.analysis;
 
@@ -12,64 +12,64 @@ import java.util.Stack;
 
 /**
  * 可排序数据对象
- * 
+ *
  * @author shutong.dy
  * @since 2012-1-11
  */
 public class TimeSortData implements Comparable<TimeSortData> {
 
-	private long sum = 0;
-	private String methodName;
-	private Stack<Long> valueStack = new Stack<Long>();
+    private long sum = 0;
+    private String methodName;
+    private Stack<Long> valueStack = new Stack<Long>();
 
-	/**
-	 * @return the methodName
-	 */
-	public String getMethodName() {
-		return methodName;
-	}
+    /**
+     * @return the methodName
+     */
+    public String getMethodName() {
+        return methodName;
+    }
 
-	/**
-	 * @param methodName
-	 *            the methodName to set
-	 */
-	public void setMethodName(String methodName) {
-		this.methodName = methodName;
-	}
+    /**
+     * @param methodName the methodName to set
+     */
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
 
-	/**
-	 * @return the valueStack
-	 */
-	public Stack<Long> getValueStack() {
-		return valueStack;
-	}
+    /**
+     * @return the valueStack
+     */
+    public Stack<Long> getValueStack() {
+        return valueStack;
+    }
 
-	/**
-	 * @param useTime
-	 * 
-	 */
-	public void addStackValue(long useTime) {
-		valueStack.add(useTime);
-		sum += useTime;
-	}
+    /**
+     * @param useTime
+     */
+    public void addStackValue(long useTime) {
+        valueStack.add(useTime);
+        sum += useTime;
+    }
 
-	/**
-	 * @return the sum
-	 */
-	public long getSum() {
-		return sum;
-	}
+    /**
+     * @return the sum
+     */
+    public long getSum() {
+        return sum;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Comparable#compareTo(java.lang.Object)
-	 */
-	public int compareTo(TimeSortData o) {
-		if (this.sum > o.sum) {
-			return -1;
-		} else {
-			return 1;
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(TimeSortData o) {
+        if (this.sum > o.sum) {
+            return -1;
+        } else if (this.sum < o.sum) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
